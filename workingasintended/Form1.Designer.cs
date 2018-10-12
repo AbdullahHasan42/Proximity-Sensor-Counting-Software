0ad05709ae8d4084ae79f6702f00a33a714036d4
Abdullah_Hasan <abdullahtaha11@yahoo.com>
2018-10-13 00:07:46 +0200

    - Added Logging

--- workingasintended/Form1.Designer.cs
+++ workingasintended/Form1.Designer.cs
@@ -29,69 +29,160 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
+            this.send_button = new System.Windows.Forms.Button();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.restart_button = new System.Windows.Forms.Button();
+            this.percentage_completion = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // send_button
             // 
-            this.button1.Location = new System.Drawing.Point(226, 180);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Send Counter";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.send_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.send_button.Location = new System.Drawing.Point(12, 52);
+            this.send_button.Name = "send_button";
+            this.send_button.Size = new System.Drawing.Size(145, 44);
+            this.send_button.TabIndex = 0;
+            this.send_button.Text = "Send";
+            this.send_button.UseVisualStyleBackColor = true;
+            this.send_button.Click += new System.EventHandler(this.Send_button__Click);
             // 
             // serialPort1
             // 
             this.serialPort1.PortName = "COM6";
-            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(467, 180);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(187, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "LED OFF, COUNTER STOP";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.richTextBox1.Location = new System.Drawing.Point(226, 55);
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(305, 41);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(190, 55);
+            this.richTextBox1.Size = new System.Drawing.Size(339, 88);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(222, 32);
+            this.label1.Location = new System.Drawing.Point(301, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Counter";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(159, 233);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // connect_button
+            // 
+            this.connect_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect_button.Location = new System.Drawing.Point(12, 220);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(141, 44);
+            this.connect_button.TabIndex = 6;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.Connect_button_Click_1);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(216, 34);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(439, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(301, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Time Elapsed:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(305, 159);
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Value = 1;
+            // 
+            // restart_button
+            // 
+            this.restart_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restart_button.Location = new System.Drawing.Point(551, 220);
+            this.restart_button.Name = "restart_button";
+            this.restart_button.Size = new System.Drawing.Size(93, 34);
+            this.restart_button.TabIndex = 11;
+            this.restart_button.Text = "RESTART";
+            this.restart_button.UseVisualStyleBackColor = true;
+            this.restart_button.Click += new System.EventHandler(this.Restart_button_Click);
+            // 
+            // percentage_completion
+            // 
+            this.percentage_completion.AutoSize = true;
+            this.percentage_completion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.percentage_completion.Location = new System.Drawing.Point(453, 185);
+            this.percentage_completion.Name = "percentage_completion";
+            this.percentage_completion.Size = new System.Drawing.Size(32, 20);
+            this.percentage_completion.TabIndex = 12;
+            this.percentage_completion.Text = "0%";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(656, 276);
+            this.Controls.Add(this.percentage_completion);
+            this.Controls.Add(this.restart_button);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.send_button);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Quality Testing Device Controls";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,11 +190,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button send_button;
         public System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button restart_button;
+        private System.Windows.Forms.Label percentage_completion;
     }
 }
 
--- workingasintended/Form1.cs
+++ workingasintended/Form1.cs
@@ -1,52 +1,177 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
 using System.Windows.Forms;
 
 namespace workingasintended
 {
     public partial class Form1 : Form
     {
+        string counter = "";
+        string inputvalue = "";
+        bool isConnected = false;
+        string[] ports;
+        int ticks = 0;
+        int day = 0;
+        int counter_end = 0;
+
         public Form1()
         {
             InitializeComponent();
-            serialPort1.Open();
-        }
+            getAvailableComPorts();
+            send_button.Enabled = false;
+            richTextBox2.Enabled = false;
+            richTextBox1.Enabled = false;
+            timer1.Stop();
 
-        public void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
-        {
-            string counter = serialPort1.ReadLine(); // ReadLine includes the + "\n"
-            displayToWindow(counter);
-        }
-
-        private void displayToWindow(string counter)
-        {
-            BeginInvoke(new EventHandler(delegate
+            foreach (string port in ports)
             {
-                richTextBox1.Text = counter;
-                //richTextBox1.ScrollToCaret();
-            }));
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
+                comboBox1.Items.Add(port);
+                if (ports[0] != null)
+                {
+                    comboBox1.SelectedItem = ports[0];
+                }
+            }
             
-            serialPort1.Write("1000000");
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Restart_button_Click(object sender, EventArgs e)
         {
-            serialPort1.Write("OFF");
+            serialPort1.Write("RESTART");
+            Re_declare_variables();
+            Re_enable_controls();
+            counter_end += 1;
         }
 
-        public void richTextBox1_TextChanged(object sender, EventArgs e)
+        private void Progressincrease()
         {
-            //richTextBox1.AppendText(serialPort1.ReadLine());
+            BeginInvoke(new EventHandler(delegate{progressBar1.PerformStep();}));
         }
+
+        private void DisplayToWindow(string counter)
+        {
+            BeginInvoke(new EventHandler(delegate{richTextBox1.Text = counter;}));
+        }
+        
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+        
+        private void Send_button__Click(object sender, EventArgs e)
+        {
+            inputvalue = richTextBox2.Text;
+            serialPort1.Write(inputvalue + "\n");
+            counter_end = Int32.Parse(inputvalue);
+            progressBar1.Maximum = counter_end + 1;
+            send_button.Enabled = false;
+            richTextBox1.Enabled = true;
+            richTextBox2.Enabled = false;
+            timer1.Start();
+        }
+
+        private void Connect_button_Click_1(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                ConnectToArduino();
+            }
+            else
+            {
+                DisconnectFromArduino();
+            }
+        }
+
+        private void ConnectToArduino()
+        {
+            try{
+                isConnected = true;
+                string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+                serialPort1.PortName = selectedPort;
+                serialPort1.Open();
+                connect_button.Text = "Disconnect";
+                send_button.Enabled = true;
+                richTextBox2.Enabled = true;
+            }
+            catch(ArgumentException)
+            {
+                MessageBox.Show("No COM Ports Selected!");
+            }
+        }
+        
+        private void DisconnectFromArduino()
+        {
+            isConnected = false;
+            richTextBox2.Text = "";
+            serialPort1.Close();
+            connect_button.Text = "Connect";
+        }
+
+        private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            counter = serialPort1.ReadLine(); // ReadLine includes the + "\n"
+
+            if (counter.StartsWith("Counter ="))
+            {
+                DisplayToWindow(counter);
+                Progressincrease();
+            }
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == counter_end)
+            {
+                timer1.Stop();
+                //create_log();
+                MessageBox.Show("Test Completed!");
+            }
+            else
+            {
+                ticks++;
+            }
+            if (ticks % 86400 == 0)
+            {
+                day++;
+            }
+
+            TimeSpan time_span = TimeSpan.FromSeconds(ticks);
+            string timer_label = time_span.ToString("%d") + " day(s), " + time_span.ToString(@"hh\:mm\:ss");
+            label2.Text = timer_label;
+            string percentage = ((progressBar1.Value / counter_end) * 100).ToString() + "%";
+            percentage_completion.Text = percentage;
+        }
+        
+        private void Re_declare_variables()
+        {
+            counter = "";
+            inputvalue = "";
+            ticks = 0;
+            day = 0;
+            counter_end = 0;
+            progressBar1.Value = 1;
+            progressBar1.Maximum = 3;
+            percentage_completion.Text = "0%";
+            richTextBox2.ResetText();
+            richTextBox1.ResetText();
+        }
+
+        private void Re_enable_controls()
+        {
+            send_button.Enabled = true;
+            richTextBox2.Enabled = true;
+        }
+        /*
+        private void create_log() 
+        {
+            //Found in the bin folder, with the .exe file
+
+            string log_date = DateTime.Now.ToString("dd_MM_yyyy");
+            StreamWriter Logger = new StreamWriter("TEST LOG " + log_date + ".txt");
+            Logger.Write("THIS IS A TEST LOG!");
+            Logger.Close();
+        }
+        */
     }
 }
